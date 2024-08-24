a205aed023981bef755a90c94ff5902a9eb98835
shs <guichanist@gmail.com>
2024-08-25 01:08:39 +0900

    방 접속 시 방 정보 보내기 구현. 방 나갈땐 메시지가 전송 안되는 문제 있음

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from room.models import Room
+from room.serializers import RoomSerializer
+from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -9,10 +12,33 @@ class ChatConsumer(AsyncWebsocketConsumer):
         # 방 그룹에 참여하기
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
+        # 데이터베이스에서 방 데이터 가져오기
+        room_data = await self.get_room_data(self.room_name)
+
+        # 방 그룹에 연결 메시지와 함께 방 데이터 보내기
+        if room_data:
+            await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": json.dumps(room_data),
+            },
+            )
+
         await self.accept()
 
     async def disconnect(self, close_code):
-        # 방 그룹에서 나가기
+        # 데이터베이스에서 방 데이터 가져오기
+        room_data = await self.get_room_data(self.room_name)
+        # 방 그룹에 연결 해제 메시지와 함께 방 데이터 보내기
+        if room_data:
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "chat.message",
+                    "message": json.dumps(room_data),
+                },
+            )
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # WebSocket으로부터 메시지 받기
@@ -31,3 +57,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # WebSocket으로 메시지 보내기
         await self.send(text_data=json.dumps({"message": message}))
+
+    @database_sync_to_async
+    def get_room_data(self, room_name):
+        try:
+            room = Room.objects.select_related().get(title=room_name)
+            serializer = RoomSerializer(room)
+            return serializer.data
+        except Room.DoesNotExist:
+            return None
