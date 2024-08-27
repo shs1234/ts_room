aec2f0106505e29b04ac663e3fe58b60916c2f18
shs <shs@shs.shs>
2024-08-27 13:17:24 +0000

    save

--- chat/consumers.py
+++ chat/consumers.py
@@ -11,91 +11,112 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        # 방 그룹에 참여하기
+        # 방 존재 여부 확인
+        room_data = await self.get_room_data(self.room_name)
+        if room_data == None :
+            await self.send(text_data=json.dumps({"message": "Room does not exist."}))
+            await self.close()
+
+        # 방 그룹에 연결
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
+        # 방 참가(DB 업데이트)
         await self.action_join(self.room_name, self.scope["user"].username)
-        room_data = await self.get_room_data(self.room_name)
+
+        # 방 참가 메시지 전송
         message = {
             "action": "join",
             "text": f"{self.scope['user'].username} has joined the room.",
             "room_data": room_data,
         }
-        # 방 그룹에 연결 메시지와 함께 방 데이터 보내기
-        if room_data:
-            await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat.message",
-                "message": json.dumps(message),
-            })
+        await self.channel_layer.group_send(
+        self.room_group_name,
+        {
+            "type": "chat.message",
+            "message": json.dumps(message),
+        })
+
+        # WebSocket 연결 수락
         await self.accept()
 
     async def disconnect(self, close_code):
+        # 방 그룹에서 나가기(DB 업데이트)
         await self.action_leave(self.room_name, self.scope["user"].username)
+
+        # 방 나가기 메시지 전송
         room_data = await self.get_room_data(self.room_name)
         message = {
             "action": "leave",
             "text": f"{self.scope['user'].username} has left the room.",
             "room_data": room_data,
         }
-        # 방 그룹에 연결 해제 메시지와 함께 방 데이터 보내기
-        if room_data:
-            await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat.message",
-                "message": json.dumps(message),
-            })
+        await self.channel_layer.group_send(
+        self.room_group_name,
+        {
+            "type": "chat.message",
+            "message": json.dumps(message),
+        })
 
+        # 방 그룹에서 연결 해제
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # WebSocket으로부터 메시지 받기
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-
         action = text_data_json["action"]
+
+        # 방 그룹에 단순 메시지 보내기
         if action == "message":
             message = text_data_json["message"]
-
-            # 방 그룹에 메시지 보내기
             await self.channel_layer.group_send(
                 self.room_group_name, {"type": "chat.message", "message": message}
             )
 
+        # start 버튼 클릭 시
         elif action == "start":
             await self.action_start(self.room_name)
             room_data = await self.get_room_data(self.room_name)
             text = f"{self.scope['user'].username} has started the game."
+        
+        # ready 버튼 클릭 시
         elif action == "ready":
             await self.action_ready(self.room_name)
             room_data = await self.get_room_data(self.room_name)
             text = f"{self.scope['user'].username} is ready."
+
+        # 방 그룹에 메시지 보내기
         message = {
             "action": action,
             "text": text,
             "room_data": room_data,
         }
-        if room_data:
-            await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "chat.message",
-                "message": json.dumps(message),
-            })
+        await self.channel_layer.group_send(
+        self.room_group_name,
+        {
+            "type": "chat.message",
+            "message": json.dumps(message),
+        })
 
     # 방 그룹으로부터 메시지 받기
     async def chat_message(self, event):
         message = event["message"]
 
         # WebSocket으로 메시지 보내기
-        await self.send(text_data=json.dumps({"message": message}))
-        # await self.send(text_data=json.dumps(event))
+        await await self.send(text_data=json.dumps({"message": message}))
+
+
+
+
+
+
+
+
+
 
     @database_sync_to_async
     def get_room_data(self, room_name):
         try:
-            room = Room.objects.select_related().get(pk=room_name)
+            room = Room.objects.get(pk=room_name)
             serializer = RoomSerializer(room)
             return serializer.data
         except Room.DoesNotExist:
@@ -103,41 +124,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def action_join(self, room_name, username):
-        try:
-            room = Room.objects.get(pk=room_name)
-            room.add_player(username)
-        except ValidationError as e:
-            return e.detail
-    
+        room = Room.objects.get(pk=room_name)
+        room.players.append(username)
+        room.current_participants += 1
+        room.save()
+
     @database_sync_to_async
     def action_leave(self, room_name, username):
-        try:
-            room = Room.objects.get(pk=room_name)
-            room.remove_player(username)
-            room.ready_reset()
-        except ValidationError as e:
-            return e.detail
+        room = Room.objects.get(pk=room_name)
+        room.players.remove(username)
+        room.current_participants -= 1
+        room.ready_reset()
+        room.save()
 
     @database_sync_to_async
     def action_start(self, room_name):
-        try:
-            room = Room.objects.get(pk=room_name)
-            room.start_game()
-        except ValidationError as e:
-            return e.detail
+        room = Room.objects.get(pk=room_name)
+        room.isPlaying = True
+        room.save()
 
     @database_sync_to_async
     def action_ready(self, room_name):
-        try:
-            room = Room.objects.get(pk=room_name)
-            room.get_ready()
-        except ValidationError as e:
-            return e.detail
-
-    @database_sync_to_async
-    def action_ready_reset(self, room_name):
-        try:
-            room = Room.objects.get(pk=room_name)
-            room.ready_reset()
-        except ValidationError as e:
-            return e.detail
+        room = Room.objects.get(pk=room_name)
+        room.ready_participants += 1
+        room.save()
--- room/models.py
+++ room/models.py
@@ -14,45 +14,5 @@ class Room(models.Model):
     def __str__(self):
         return self.title
 
-    def add_player(self, username):
-        if self.user_in_room(username):
-            raise ValidationError("User is already in the room.")
-        if self.current_participants < self.max_participants:
-            self.players.append(username)
-            self.current_participants += 1
-            self.save()
-        else:
-            raise ValidationError("The room is full.")
-
-    def remove_player(self, username):
-        if username in self.players:
-            self.players.remove(username)
-            self.current_participants -= 1
-            self.save()
-        else:
-            raise ValidationError("User is not in the room.")
-
-    def start_game(self):
-        if self.current_participants < self.max_participants:
-            raise ValidationError("The room is not full.")
-        if self.ready_participants < self.max_participants:
-            raise ValidationError("The players are not ready.")
-        self.isPlaying = True
-        self.save()
-    
-    def get_ready(self):
-        if self.isPlaying:
-            raise ValidationError("The game has already started.")
-        if self.current_participants < self.max_participants:
-            raise ValidationError("The room is not full.")
-        self.ready_participants += 1
-        self.save()
-
-    def ready_reset(self):
-        if self.isPlaying:
-            raise ValidationError("The game has already started.")
-        self.ready_participants = 0
-        self.save()
-
     def user_in_room(self, username):
         return Room.objects.filter(players__contains=[username]).exists()
